fd1dc3b0a20c7e59cb61967932813e5f88f1cc38
Lucas Serafim <lucashenrique74148@gmail.com>
2025-04-11 14:57:01 -0300

    feat:recriando codigo para funcionamento

--- memotecas/exercicio8/src/app/componentes/formularios/formularios.component.ts
+++ memotecas/exercicio8/src/app/componentes/formularios/formularios.component.ts
@@ -1,11 +1,45 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, Validators, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
-  selector: 'app-formularios',
-  imports: [],
+  selector: 'app-forms-validation',
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './formularios.component.html',
-  styleUrl: './formularios.component.css'
+  styleUrls: ['./formularios.component.css']
 })
-export class FormulariosComponent {
+export class FormsValidationComponent {
+  formulario = new FormGroup({
+    nome: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    telefone: new FormControl('', [Validators.required, Validators.pattern(/^\d{10,11}$/)]),
+    data: new FormControl('', [Validators.required])
+  });
 
+  // Getters para facilitar o acesso aos controles no HTML
+  get nome() {
+    return this.formulario.get('nome')!;
+  }
+
+  get email() {
+    return this.formulario.get('email')!;
+  }
+
+  get telefone() {
+    return this.formulario.get('telefone')!;
+  }
+
+  get data() {
+    return this.formulario.get('data')!;
+  }
+
+  onSubmit() {
+    if (this.formulario.valid) {
+      console.log('Formulário válido', this.formulario.value);
+    } else {
+      console.log('Formulário inválido');
+      this.formulario.markAllAsTouched(); // Marca todos os campos como tocados para exibir erros
+    }
+  }
 }
