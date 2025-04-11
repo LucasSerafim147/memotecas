4d59bb724cc39576dfb6c8bc67c40f0560a150ea
Lucas Serafim <lucashenrique74148@gmail.com>
2025-04-11 15:47:47 -0300

    fix: ajustando validation

--- memotecas/exercicio8/src/app/app.component.ts
+++ memotecas/exercicio8/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsValidationComponent } from "./componentes/formularios/formularios.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsValidationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
