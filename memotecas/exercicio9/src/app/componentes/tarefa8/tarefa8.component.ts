a64e4c3c3e4d6e50b0b26fa1ac113a5a0c3a8a08
Lucas Serafim <lucashenrique74148@gmail.com>
2025-04-14 12:46:00 -0300

    feat: adicionado ngOn init e formgroup

--- memotecas/exercicio9/src/app/componentes/tarefa8/tarefa8.component.ts
+++ memotecas/exercicio9/src/app/componentes/tarefa8/tarefa8.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, Validators, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-tarefa8',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './tarefa8.component.html',
   styleUrl: './tarefa8.component.css'
 })
 export class Tarefa8Component {
-
+  formulario = new FormGroup({
+    nome: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    instituicao: new FormControl('', [Validators.required, Validators.min(3)]),
+})
+onSubmit() {
+  if (this.formulario.valid) {
+    console.log('Formulário válido', this.formulario.value);
+  } else {
+    console.log('Formulário inválido');
+    this.formulario.markAllAsTouched(); 
+  }
+}
 }
