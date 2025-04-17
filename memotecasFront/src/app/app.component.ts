83535e650712ce58fd69838b134b8027a6e1e125
Lucas Serafim <lucashenrique74148@gmail.com>
2025-04-17 16:46:13 -0300

    feat: adicioando forms

--- memotecasFront/src/app/app.component.ts
+++ memotecasFront/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./shared/header/header.component";
 import { FooterComponent } from "./shared/rodape/footer/footer.component";
+import { FormularioComponent } from "./pages/formulario/formulario.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, FormularioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
