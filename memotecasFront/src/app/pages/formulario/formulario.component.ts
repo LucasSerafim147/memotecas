81f2eddb8d2908f5b999aed745ae7daab5e77128
Lucas Serafim <lucashenrique74148@gmail.com>
2025-04-20 11:49:39 -0300

    feat: adicionando pensamento service

--- memotecasFront/src/app/pages/formulario/formulario.component.ts
+++ memotecasFront/src/app/pages/formulario/formulario.component.ts
@@ -1,11 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-formulario',
-  imports: [],
+  imports: [CommonModule, RouterModule,],
   templateUrl: './formulario.component.html',
   styleUrl: './formulario.component.css'
 })
 export class FormularioComponent {
+  /**
+   *
+   */
+  constructor() {
+    
+    
+  }
 
 }
---
+++ memotecasFront/src/app/shared/service/pensamentos.service.ts
@@ -0,0 +1,37 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+
+export interface Pensamento{
+  id?: number,
+  pensamento:string,
+  autor:string,
+  modelo:number
+}
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PensamentosService {
+  private readonly API =  'https://localhost:7177/swagger/index.html'
+
+  constructor(private http: HttpClient) { }
+  listar(): Observable<Pensamento[]> {
+    return this.http.get<Pensamento[]>(this.API);
+  }
+
+  criar(pensamento: Pensamento): Observable<Pensamento> {
+    return this.http.post<Pensamento>(this.API, pensamento);
+  }
+
+  deletar(id: number): Observable<any> {
+    return this.http.delete(`${this.API}/${id}`);
+  }
+
+  atualizar(pensamento: Pensamento): Observable<Pensamento> {
+    return this.http.put<Pensamento>(`${this.API}/${pensamento.id}`, pensamento);
+  }
+}
+
